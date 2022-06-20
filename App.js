8577e0ce481cf6706d5067557c1c924ac3db1666
JewelryKim <jsjs21good@gmail.com>
2022-06-20 11:15:06 +0900

    Updates

---
+++ App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import EngineSample from "./EngineSample";
+import Home from "./view/home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
